c167c69baae0c7b8a99540e3535cbf0538f05923
tallner <ctallner@hotmail.com>
2021-12-09 21:25:39 +0100

    a few posts

--- projects/Tenta/src/main/java/com/cte/postservlet.java
+++ projects/Tenta/src/main/java/com/cte/postservlet.java
@@ -1,6 +1,8 @@
 package com.cte;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,20 +15,82 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/postservlet")
 public class postservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public postservlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public postservlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		out.print("<html><body>");
+		out.print("<h3>Hello Servlet with POST</h3>");
+
+		String pword = request.getParameter("pword");
+		String uname = request.getParameter("uname");
+		int loginLevel = Integer.parseInt(request.getParameter("usertype"));
+		
+		String loginresponse = "false";
+		
+		//out.print(pass);
+		if (
+				(uname.equals("christian"))
+				&& (pword.equals("admin")) 
+				&& (loginLevel == 3)
+				) 
+		{
+			out.print("<h3>Welcome in admin</h3>"
+					+ "<br/>"
+					+ "<div>"
+					+ "<form action=\"http://localhost:8080/Tenta/index.jsp?inlogfail=false\" method=\"post\">"
+					+ "<input type=\"submit\" value=\"HOME\">"
+					+ "</form>"
+					+ "</div>");
+			
+
+		} else if (
+				(uname.equals("daria"))
+				&& (pword.equals("service")) 
+				&& (loginLevel == 2)
+				)
+		{
+			out.print("<h3>Welcome in service</h3>");
+		}  else if (
+				(uname.equals("john"))
+				&& (pword.equals("user")) 
+				&& (loginLevel == 1)
+				)
+		{
+			out.print("<h3>Welcome in user</h3>");
+		} else {
+
+			System.out.println("wrond pass");
+			out.print("<h3>NOPE</h3>");
+			loginresponse = "true";
+			response.sendRedirect("index.jsp?inlogfail="+loginresponse);
+		}
+
+		// methods works
+
+		out.print("<p>");
+//		DoDog(out);
+		// have to send the printer with the method of to print HTML over there
+
+		out.print("</p>");
+
+		out.close();
 	}
 
 }
--- projects/Tenta/src/main/webapp/index.jsp
+++ projects/Tenta/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,49 @@
 <title>Insert title here</title>
 </head>
 <body>
+
+	<div>
+		<form action="getservlet" method="get">
+			How many dogs do you want? <input name="nrdogs" type="text">
+<!-- 			 <select name="stuff"> -->
+<!-- 				<option value="10">10</option> -->
+<!-- 				<option value="200">200</option> -->
+<!-- 			</select>  -->
+			<input type="submit" value="Print my dogs!">
+		</form>
+	</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+	<div>
+		<form action="postservlet" method="post">
+			Enter name: <input name="uname" type="text">
+			<br>
+			Enter password: <input name="pword" type="password"> 
+			<br>
+			Usertype:
+			<select name="usertype">
+				<option value="1">User</option>
+				<option value="2">Service</option>
+				<option value="3">Admin</option>
+			</select> 
+			<br>
+			<input type="submit" value="Sign in">
+		</form>
+	</div>
+	<% 
+		String loginFailed = request.getParameter("inlogfail") == null ? "false" : request.getParameter("inlogfail");
+		if(loginFailed.equals("true"))
+		{
+			out.print("<h2>Login failed, try again!</h2>");
+		} else out.print("");
+	%>
+	
+	
 
 </body>
 </html>
